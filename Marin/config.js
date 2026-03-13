1e7c7b742e1bf661140681f0072a3a943de66e2d
rajdevmyvps0w <rajdevmyvps02@gmail.com>
2026-03-13 18:21:30 +0530

    Update config.js

--- Marin/config.js
+++ Marin/config.js
@@ -11,9 +11,9 @@ if (!gg) {
 // -------------------------------------------------------------- //
 
 global.owner = gg.split("918434573266");
-global.mongodb = process.env.MONGODB || "mongodb+srv://Stenx:Stenx001@cluster0.yp36fqr.mongodb.net/?appName=Cluster0";
-global.sessionId = process.env.SESSION_ID || "okaytoo96";
-global.prefa = process.env.PREFIX || "#";
+global.mongodb = process.env.MONGODB || "";
+global.sessionId = process.env.SESSION_ID || "";
+global.prefa = process.env.PREFIX || "!";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
 global.packname = process.env.PACKNAME || `Marin MD`;
 global.author = process.env.AUTHOR || "by: Sten-X";
